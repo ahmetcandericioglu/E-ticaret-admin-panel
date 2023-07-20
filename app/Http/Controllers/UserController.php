ebda10eda131c3c828b13cadfe1ec6c7a893371f
Ahmet Can DERİCİOĞLU <4hcan2001@gmail.com>
2023-07-20 10:37:02 +0300

    kullanıcı eklerken password hashleme özelliği eklendi

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -48,7 +49,7 @@ class UserController extends Controller
         User::create([
             'usertitle' => $request->usertitle,
             'username' => $request->username,
-            'password' => $request->password,
+            'password' => Hash::make($request->password),
         ]);
 
         return view('user_registerpage');
