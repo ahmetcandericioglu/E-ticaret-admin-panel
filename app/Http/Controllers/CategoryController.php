8850619ee7811005ff79f6cba6ecc5bff126d3c9
Ahmet Can DERİCİOĞLU <4hcan2001@gmail.com>
2023-07-27 10:10:35 +0300

    category silme işlemindeki hata düzeltildi

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,18 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('categorypage');
     }
 
-    public function toAddCategory(){
+    public function toAddCategory()
+    {
         return view('category_add');
     }
-    public function addCategory(Request $request){
+
+    public function addCategory(Request $request)
+    {
         $request->validate([
             'categorytitle' => 'required|unique:category',
             'categorydescription' => 'required',
@@ -22,23 +27,31 @@ class CategoryController extends Controller
         ]);
 
         Category::create([
-           'categorytitle' => $request->categorytitle,
-           'categorydescription' => $request->categorydescription,
-           'categorystatus' => $request->categorystatus,
+            'categorytitle' => $request->categorytitle,
+            'categorydescription' => $request->categorydescription,
+            'categorystatus' => $request->categorystatus,
         ]);
 
         return redirect()->route('add_category');
     }
 
-    public function toCategoryList(){
+    public function toCategoryList()
+    {
         $categories = Category::all();
         return view('category_listpage', ["categories" => $categories]);
     }
 
-    public function deleteCategories(Request $request){
+    public function deleteCategories(Request $request)
+    {
         $ids = $request->selectedids;
-        if (!($ids == null))
+        if (!($ids == null)) {
+            $categoryProducts = Product::query()->whereIn("productcategoryid", $ids)->get();
+            foreach ($categoryProducts as $product) {
+                $product->productcategoryid = null;
+                $product->save();
+            }
             Category::whereIn('id', $ids)->delete();
+        }
         return redirect()->route('list_category');
     }
 }
