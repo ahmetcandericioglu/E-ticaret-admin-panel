de84222aa718087fbf54c5ce7e855d09b42ef9f5
Ahmet Can DERİCİOĞLU <4hcan2001@gmail.com>
2023-08-10 16:53:50 +0300

    routelar düzenlendi

--- routes/web.php
+++ routes/web.php
@@ -32,41 +32,56 @@ Route::group(['middleware' => 'auth'], function () {
         return view('homepage');
     })->name('home');
 
-    Route::group(['prefix' => '/user'], function () {
-        Route::get('/', [UserController::class, 'toUser'])->name('user');
-        Route::get('/add-user', [UserController::class, 'toRegister'])->name('register');
-        Route::post('/add-user', [UserController::class, 'registerControl'])->name('register_user');
-        Route::get('/userlist', [UserController::class, 'toUserList'])->name('userlist');
-        Route::get('/edit/{user}', [UserController::class, 'toUpdateUser'])->name('edit');
-        Route::post('/edit/{user}', [UserController::class, 'toUpdateUser'])->name('update');
-        Route::get('/delete/{user}', [UserController::class, 'toDeleteUser'])->name('delete');
-        Route::post('/delete-user/{user}', [UserController::class, 'deleteUser'])->name('delete_user');
-        Route::post('/delete-user', [UserController::class, 'deleteUsers'])->name('delete_users');
-        Route::post('/edit-user/{user}', [UserController::class, 'editUser'])->name('edit_user');
+    Route::group(
+        [
+            'prefix' => '/user',
+            'controller' => UserController::class,
+        ],
+        function () {
+        Route::get('/', 'toUser')->name('user');
+        Route::get('/add-user', 'toRegister')->name('register');
+        Route::post('/add-user', 'registerControl')->name('register_user');
+        Route::get('/userlist', 'toUserList')->name('userlist');
+        Route::get('/edit/{user}', 'toUpdateUser')->name('edit');
+        Route::post('/edit/{user}', 'toUpdateUser')->name('update');
+        Route::get('/delete/{user}', 'toDeleteUser')->name('delete');
+        Route::post('/delete-user/{user}', 'deleteUser')->name('delete_user');
+        Route::post('/delete-user', 'deleteUsers')->name('delete_users');
+        Route::post('/edit-user/{user}', 'editUser')->name('edit_user');
     }
     );
-    Route::group(['prefix' => '/category'], function () {
-        Route::get('/', [CategoryController::class, 'index'])->name('category');
-        Route::get('/add-category', [CategoryController::class, 'toAddCategory'])->name('add_category');
-        Route::post('/add-category', [CategoryController::class, 'addCategory'])->name('add_new_category');
-        Route::get('/list-category', [CategoryController::class, 'toCategoryList'])->name('list_category');
-        Route::get('/edit-category/{category}', [CategoryController::class, 'toEditCategory'])->name('edit_category');
-        Route::post('/edit-category/{category}', [CategoryController::class, 'editCategory'])->name('update_category');
-        Route::get('/delete-category/{category}', [CategoryController::class, 'toDeleteCategory'])->name('delete_category');
-        Route::post('/delete-category/{category}', [CategoryController::class, 'deleteCategory'])->name('delete_category.post');
-        Route::post('/delete-categories', [CategoryController::class, 'deleteCategories'])->name('delete_categories');
+    Route::group(
+        [
+            'prefix' => '/category',
+            'controller' => CategoryController::class,
+        ],
+        function () {
+        Route::get('/', 'index')->name('category');
+        Route::get('/add-category','toAddCategory')->name('add_category');
+        Route::post('/add-category','addCategory')->name('add_new_category');
+        Route::get('/list-category','toCategoryList')->name('list_category');
+        Route::get('/edit-category/{category}','toEditCategory')->name('edit_category');
+        Route::post('/edit-category/{category}','editCategory')->name('update_category');
+        Route::get('/delete-category/{category}','toDeleteCategory')->name('delete_category');
+        Route::post('/delete-category/{category}','deleteCategory')->name('delete_category.post');
+        Route::post('/delete-categories','deleteCategories')->name('delete_categories');
     }
     );
-    Route::group(['prefix' => '/product'], function () {
-        Route::get('/', [ProductController::class, 'index'])->name('product');
-        Route::get('/add-product', [ProductController::class, 'toAddProduct'])->name('add_product');
-        Route::post('/add-product', [ProductController::class, 'addProduct'])->name('add_new_product');
-        Route::get('/list-product', [ProductController::class, 'toProductList'])->name('list_product');
-        Route::get('/edit-product/{product}', [ProductController::class, 'toEditProduct'])->name('edit_product');
-        Route::post('/edit-product/{product}', [ProductController::class, 'editProduct'])->name('update_product');
-        Route::get('/delete-product/{product}', [ProductController::class, 'toDeleteProduct'])->name('delete_product');
-        Route::post('/delete-product/{product}', [ProductController::class, 'deleteProduct'])->name('delete_product.post');
-        Route::post('/delete-products', [ProductController::class, 'deleteProducts'])->name('delete_products');
+    Route::group(
+        [
+            'prefix' => '/product',
+            'controller' => ProductController::class,
+        ],
+        function () {
+        Route::get('/', 'index')->name('product');
+        Route::get('/add-product', 'toAddProduct')->name('add_product');
+        Route::post('/add-product', 'addProduct')->name('add_new_product');
+        Route::get('/list-product', 'toProductList')->name('list_product');
+        Route::get('/edit-product/{product}', 'toEditProduct')->name('edit_product');
+        Route::post('/edit-product/{product}', 'editProduct')->name('update_product');
+        Route::get('/delete-product/{product}', 'toDeleteProduct')->name('delete_product');
+        Route::post('/delete-product/{product}', 'deleteProduct')->name('delete_product.post');
+        Route::post('/delete-products', 'deleteProducts')->name('delete_products');
     }
     );
 });
